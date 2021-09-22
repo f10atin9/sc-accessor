52b46c5df7a3f5f52b901892cee77e08fe32aca8
f10atin9 <f10atin9@kubesphere.io>
2021-09-22 15:53:00 +0800

    init demo
    Signed-off-by: f10atin9 <f10atin9@kubesphere.io>

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"k8s.io/apimachinery/pkg/runtime"
 	"log"
@@ -17,31 +16,45 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/pvc", pvcHandler)
+
+	// Listening create pvc request
+	log.Println("start listening")
+
+	http.HandleFunc("*/pvc", pvcCreateHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func pvcHandler(w http.ResponseWriter, r *http.Request) {
+func pvcCreateHandler(w http.ResponseWriter, r *http.Request) {
+	// read request
 	reqBody, err := ioutil.ReadAll(r.Body)
 	defer r.Body.Close()
-
+	var msg string
 	if err != nil {
-		log.Println("read request body failed")
+		msg = "read request body failed"
+		log.Println(msg)
+		w.Write([]byte(msg))
 		return
 	}
 
+	//get pvc object
 	pvc := &corev1.PersistentVolumeClaim{}
 	err = json.Unmarshal(reqBody, pvc)
 	if err != nil {
-		log.Println("unmarshal json failed")
+		msg = "unmarshal json failed"
+		log.Println(msg)
+		w.Write([]byte("unmarshal json failed"))
 		return
 	}
+	storageClass := *pvc.Spec.StorageClassName
+	namespace := pvc.Namespace
 
+	//init accessor client
 	cfg, err := config.GetConfig()
 	if err != nil {
-		log.Println("getConfig err:", err)
+		msg = "getConfig err:" + err.Error()
+		log.Println(msg)
+		w.Write([]byte(msg))
 	}
-
 	var cli1 client.Client
 	opts := client.Options{}
 	scheme := runtime.NewScheme()
@@ -49,38 +62,42 @@ func pvcHandler(w http.ResponseWriter, r *http.Request) {
 	opts.Scheme = scheme
 	cli1, err = client.New(cfg, opts)
 
-	storageClass := *pvc.Spec.StorageClassName
-	namespace := pvc.Namespace
-
+	//get accessor by pvc storageClassName
 	accessor := &v1alpha1.Accessor{}
-
 	err = cli1.Get(
 		context.Background(),
 		types.NamespacedName{Namespace: "", Name: storageClass + "-accessor"},
 		accessor,
 	)
 	if err != nil {
-		log.Println("get accessor failed:", err)
+		msg = "get accessor failed:" + err.Error()
+		log.Println(msg)
+		w.Write([]byte(msg))
 		return
 	}
-	fmt.Println("hello")
-	for _, ele := range accessor.Spec.AllowedNamespace {
-		fmt.Println("ele is :", ele)
-		if ele == namespace {
+
+	for _, ns := range accessor.Spec.AllowedNamespace {
+		// if in allowNS, create pvc
+		if ns == namespace {
 			var pvcClient client.Client
 			pvcClient, _ = client.New(config.GetConfigOrDie(), client.Options{})
 			err := pvcClient.Create(context.Background(), pvc)
 			if err != nil {
 				w.WriteHeader(http.StatusForbidden)
-
-				log.Fatal("post req to sc create pvc failed:", err)
-
+				msg = "post req to sc create pvc failed:" + err.Error()
+				log.Fatal(msg)
+				w.Write([]byte(msg))
 				return
 			}
 			w.WriteHeader(http.StatusOK)
+			msg = storageClass + " pvc \"" + pvc.Name + "\"created in the namespace " + namespace
+			w.Write([]byte("pvc"))
 			return
 		}
 	}
-	w.WriteHeader(http.StatusBadRequest)
+
+	// else reject request
+	w.WriteHeader(http.StatusForbidden)
+	w.Write([]byte("It is not allowed to create " + storageClass + " pvc in the namespace" + namespace))
 	return
 }
